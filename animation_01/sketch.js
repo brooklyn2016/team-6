993e9b65538e844aa2a9ac156f7ce5f2f7af0a63
David Sim <ws903@nyu.edu>
2016-10-29 02:27:11 -0400

    animation_2nd_update

--- animation_01/sketch.js
+++ animation_01/sketch.js
@@ -1,18 +1,28 @@
 var x = 0;
 var drw = false;
 
+function preload() {
+  pic = loadImage("download.png");
+}
+
 function setup() {
   createCanvas(800, 600);
-  fill('red');
-  rect(0, 550, 50, 50);
+  background(173, 216, 230); // background color
+  fill('green');
+  rect(0,550,800,50);
+  image(pic, x, 500, 50, 50);
 }
 
 function draw() {
   if(drw == true) {
   background(255); // one number 0-255 is dark to light
   x = x + 3;
-  fill('red');
-  rect(x, 550, 50, 50); 
+  background(173, 216, 230);
+  
+  fill('green');
+  rect(0,550,800,50);
+  
+  image(pic, x, 500, 50, 50); 
 }
 }
 
